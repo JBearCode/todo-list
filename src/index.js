744f025d7da8824d667780cd3853f6988d7f8f64
JBearCode <joelbmoran@gmail.com>
2021-05-24 16:42:03 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+console.log('webpack is working');
+
+class todoMaker {
+    constructor(title, description, created, due, urgent, complete) {
+        this.title = title;
+        this.description = description;
+        this.created = created;
+        this.due = due;
+        this.urgent = urgent;
+        this.complete = complete;
+    };
+};
